68185c11898aff9f5c0aa2d932aecbf473d41a2e
FHSU-AustinHoward <A_Howard4@mail.fhsu.edu>
2024-02-07 15:28:19 -0600

    Added import statements to main updated requirements.txt to assumed final list

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 # Austin Howard
 # Mini Project 1
 
+# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
+
 # Please submit a link to your GitHub project. Do not submit your project files here.
 # This project will be using the packages NumPy and Matplotlib in order to create 5 graphs that output as PNG files.
 
-# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # (5/5 points) Proper import of packages used.
-# (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock
+# tickers for the last 10 trading days.
+
+
 # (10/10 points) Store this information in a list that you will convert to a array in NumPy.
 # (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
