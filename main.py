cbe9aa18fb66f6af96e6ca3df62edcf0f5e9aa23
FHSU-AustinHoward <A_Howard4@mail.fhsu.edu>
2024-02-07 15:57:31 -0600

    Added lists and fetched unformatted stock data

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
+# Please submit a link to your GitHub project. Do not submit your project files here.
+# This project will be using the packages NumPy and Matplotlib in order to create 5 graphs that output as PNG files.
+
+# COMPLETE - (5/5 points) Initial comments with your name, class and project at the top of your .py file.
+# COMPLETE - (5/5 points) Proper import of packages used.
+# INCOMPLETE - (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your
+# favorite stock tickers for the last 10 trading days.
+# INCOMPLETE - (10/10 points) Store this information in a list that you will convert to a array in NumPy.
+# INCOMPLETE - (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like
+# exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
+# INCOMPLETE - (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your
+# project folder, the project should save these when it executes. You may want to add this folder to your .gitignore
+# file.
+# INCOMPLETE - (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+# INCOMPLETE - (10/10 points) I will be checking out the master branch of your project. Please be sure to include
+# a requirements.txt file which contains all the packages that need installed. You can create this filled with the
+# output of pip freeze at the terminal prompt.
+# INCOMPLETE - (20/20 points) There should be a README.md file in your project that explains what your project is,
+# how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
+
 # INF601 - Advanced Programming in Python
 # Austin Howard
 # Mini Project 1
 
-# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
-
-# Please submit a link to your GitHub project. Do not submit your project files here.
-# This project will be using the packages NumPy and Matplotlib in order to create 5 graphs that output as PNG files.
-
-# (5/5 points) Proper import of packages used.
+# Imports
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-# (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock
-# tickers for the last 10 trading days.
+# Lists to store selected stock tickers / closing price
+selected_tickers = ['TM', 'HMC', 'F', 'GM', 'TSLA']
+closing_prices = []
 
-
-# (10/10 points) Store this information in a list that you will convert to a array in NumPy.
-# (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
-# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
-# (10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
-# (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
+# Fetch ticker information and store it in lists
+for ticker in selected_tickers:
+    retrieved_stock_data = yf.download(ticker, period='10d')
+    print(retrieved_stock_data)
